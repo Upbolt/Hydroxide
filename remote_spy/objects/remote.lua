21b9557582e3277bfa0b740e02e8ee6acedb1918
nrv-ous <nnrv.ous@gmail.com>
2020-03-19 17:41:47 -0700

    Okay well turns out the weak tables method doesn't work properly, so I'm reverting back to my original method.

--- remote_spy/objects/remote.lua
+++ remote_spy/objects/remote.lua
@@ -1,51 +1,23 @@
-local run_service = game:GetService("RunService")
 local remote = {}
 
-local weak_table = {
-    __mode = 'v',
-    __index = function(t, i)
-        if i == 'is_destroyed' then
-            return t[1] and true
-        elseif i == "data" then
-            return t[1]
-        end
-    end
-}
-
-remote.cache = setmetatable({}, {
-    __index = function(cache, remote)
-        for object, instance in pairs(cache) do
-            if instance.data == remote then
-                return object
-            end
-        end
-    end
-})
+remote.cache = {}
 
 remote.new = function(instance)
     local object = {}
 
     object.remove = remote.remove
-    object.calls = 0
+    object.data = instance
     object.ignore = false
     object.block = false
-    object.alive_check = instance.ChildAdded:Connect(function()end)
+    object.calls = 0
+    object.logs = {}
 
-    remote.cache[object] = setmetatable({instance}, weak_table)
-
+    remote.cache[instance] = object
     return object
 end
 
 remote.remove = function(object)
-    remote.cache[object] = nil
+    remote.cache[remote.data] = nil
 end
-
-run_service.RenderStepped:Connect(function()
-    for object, instance in pairs(remote.cache) do
-        if instance.is_destroyed then
-            object:remove()
-        end
-    end
-end)
 
 return remote
