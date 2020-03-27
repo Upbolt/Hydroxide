3756906626ea2a79f242d47067992f6019d2f013
nrv-ous <nnrv.ous@gmail.com>
2020-03-27 08:53:43 -0700

    Fixed a dumb syntax error in the main scan function

--- upvalue_scanner/main.lua
+++ upvalue_scanner/main.lua
@@ -17,6 +17,7 @@ local table_upvalue = oh.import('upvalue_scanner/objects/table_upvalue')
 local ui = oh.import('upvalue_scanner/ui')
 
 local upvalue_scanner = {}
+local current_upvalues = {}
 
 local match_query = function(query, data)
     return data:find(query) ~= nil
@@ -26,10 +27,11 @@ local scan = function(query)
     local results = {}
 
     for i, func in pairs(oh.methods.get_gc()) do
-        if type(func) == "function" and not oh.methods.is_x_closure(func) do
+        if type(func) == "function" and not oh.methods.is_x_closure(func) then
             for k, upvalue in pairs(oh.methods.get_upvalues(func)) do
                 if type(upvalue) == "table" then
                     for key, value in pairs(upvalue) do
+
                         if match_query(query, key) then
                             
                         end
