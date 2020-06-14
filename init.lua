2f2936f93c11a131060a8f762ba2cea50ee83269
Upbolt <nnrv.ous@gmail.com>
2020-06-14 14:19:38 -0700

    Changed configuration to web

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ if oh then
     oh.Exit()
 end
 
-local web = false
+local web = true
 local importCache = {}
 
 local function import(asset)
@@ -114,7 +114,7 @@ environment.oh = {
         end
 
         local gmt = getrawmetatable(game)
-        
+
         if isReadOnly(gmt) then
             setReadOnly(gmt, false) 
         end
