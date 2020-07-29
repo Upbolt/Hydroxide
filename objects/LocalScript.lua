58bccf880c320ddc7faec2a9dc659f1d0593a686
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2020-07-29 11:11:47 +0100

    Temporary ScriptScanner fix for PS Api.
    Make localscript.Protos nil until Slappy adds debug.getProtos()

--- objects/LocalScript.lua
+++ objects/LocalScript.lua
@@ -7,7 +7,12 @@ function LocalScript.new(instance)
     localScript.Instance = instance
     localScript.Environment = getSenv(instance)
     localScript.Constants = getConstants(closure)
-    localScript.Protos = getProtos(closure)
+
+    if is_protosmasher_caller() then
+        localScript.Protos = {nil}
+    else
+        localScript.Protos = getProtos(closure)
+    end
 
     return localScript
 end
