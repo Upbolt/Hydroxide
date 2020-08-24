b289c129c7d3b4c5cc01145d0fdf7634a3a38869
nrv-ous <nnrv.ous@gmail.com>
2020-08-24 09:32:43 -0700

    added searchClosure to auxiliary

--- ohaux.lua
+++ ohaux.lua
@@ -5,14 +5,30 @@ local getInfo = debug.getinfo or getinfo
 
 assert(getGc and getInfo, "Your exploit is not supported")
 
-local function findClosure(name)
-    for i, v in pairs(getGc()) do
-        if type(v) == "function" and getInfo(v).name == name then
-            return v
+local function matchConstants(closure, list)
+    if not list then
+        return false
+    end
+    
+    for i,v in pairs(debug.getconstants(closure)) do
+        if not list[i] then
+            return false
+        end
+    end
+    
+    return true
+end
+
+local function searchClosure(script, name, constants)
+    for i,v in pairs(getgc()) do
+        if type(v) == "function" and rawget(getfenv(v), "script") == script then
+            if ((name and name ~= '') and debug.getinfo(v).name == name) or matchConstants(v, constants) then
+                return v
+            end
         end
     end
 end
 
-aux.findClosure = findClosure
+aux.searchClosure = searchClosure
 
 return aux
