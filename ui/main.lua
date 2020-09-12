6aebf33720cac5560557bfd4045497e99944b1d9
LiamBogur <38594963+LiamBogur@users.noreply.github.com>
2020-09-12 16:39:35 +1000

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -15,8 +15,8 @@ local ConstantScanner
 
 xpcall(function()
 --    RemoteSpy = import("ui/modules/RemoteSpy")
-    ClosureSpy = import("ui/modules/ClosureSpy")
---    ScriptScanner = import("ui/modules/ScriptScanner")
+--    ClosureSpy = import("ui/modules/ClosureSpy")
+    ScriptScanner = import("ui/modules/ScriptScanner")
 --    ModuleScanner = import("ui/modules/ModuleScanner")
 --    UpvalueScanner = import("ui/modules/UpvalueScanner")
 --    ConstantScanner = import("ui/modules/ConstantScanner")
