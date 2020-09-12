dffac9442ac69c9c71974ceee309f05a11b14cf1
LiamBogur <38594963+LiamBogur@users.noreply.github.com>
2020-09-12 16:27:36 +1000

    Further testing

--- ui/main.lua
+++ ui/main.lua
@@ -13,28 +13,28 @@ local ModuleScanner
 local UpvalueScanner
 local ConstantScanner
 
-xpcall(function()
-    RemoteSpy = import("ui/modules/RemoteSpy")
-    ClosureSpy = import("ui/modules/ClosureSpy")
-    ScriptScanner = import("ui/modules/ScriptScanner")
-    ModuleScanner = import("ui/modules/ModuleScanner")
-    UpvalueScanner = import("ui/modules/UpvalueScanner")
-    ConstantScanner = import("ui/modules/ConstantScanner")
-end, function(err)
-    local message 
-
-    if err:find("valid member") then
-        message = "The UI has updated, please rejoin and restart. If you get this message more than once, screenshot this message and report it in the Hydroxide server.\n\n" .. err
-    else
-        message = "Report this error in Hydroxide's server:\n\n" .. err
-    end
-
-    MessageBox.Show("An error has occurred", message, 
-        MessageType.OK, 
-        function()
-            Interface:Destroy() 
-        end)
-end)
+--xpcall(function()
+--    RemoteSpy = import("ui/modules/RemoteSpy")
+--    ClosureSpy = import("ui/modules/ClosureSpy")
+--    ScriptScanner = import("ui/modules/ScriptScanner")
+--    ModuleScanner = import("ui/modules/ModuleScanner")
+--    UpvalueScanner = import("ui/modules/UpvalueScanner")
+--    ConstantScanner = import("ui/modules/ConstantScanner")
+--end, function(err)
+--    local message 
+--
+--    if err:find("valid member") then
+--        message = "The UI has updated, please rejoin and restart. If you get this message more than once, screenshot this message and report it in the Hydroxide server.\n\n" .. err
+--    else
+--        message = "Report this error in Hydroxide's server:\n\n" .. err
+--    end
+--
+--    MessageBox.Show("An error has occurred", message, 
+--        MessageType.OK, 
+--        function()
+--            Interface:Destroy() 
+--        end)
+--end)
 
 local constants = {
     opened = UDim2.new(0.5, -325, 0.5, -175),
