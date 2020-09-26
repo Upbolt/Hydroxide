1af46741d87f0a76d710a654d1f42e06676e6922
nrv-ous <nnrv.ous@gmail.com>
2020-09-26 00:46:42 -0700

    added script generation for tables and table elements in the upvalue scanner

--- methods/string.lua
+++ methods/string.lua
@@ -28,7 +28,25 @@ local function toString(value)
     end
 end
 
-local toUnicode = function(string)
+local function dataToString(data)
+    local dataType = type(data)
+
+    if dataType == "string" then
+        return '"' .. data:gsub('"', '\\"') .. '"'
+    elseif dataType == "table" then
+        return tableToString(data)
+    elseif dataType == "userdata" then
+        if typeof(data) == "Instance" then
+            return getInstancePath(data)
+        end
+
+        return userdataValue(data)
+    end
+
+    return tostring(data)
+end
+
+local function toUnicode(string)
     local codepoints = "utf8.char("
     
     for _i, v in utf8.codes(string) do
@@ -39,5 +57,6 @@ local toUnicode = function(string)
 end
 
 methods.toString = toString
+methods.dataToString = dataToString
 methods.toUnicode = toUnicode
 return methods
