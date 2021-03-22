da45fa03a96e4f918cf368270402c1953631adb0
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-22 13:07:58 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local environment = assert(getgenv, "<OH> ~ Your exploit is not supported")()
 
 if oh then
-    oh.Exit()
+	oh.Exit()
 end
 
 local web = true
@@ -9,191 +9,191 @@ local user = "Upbolt" -- change if you're using a fork
 local importCache = {}
 
 local function import(asset)
-    if importCache[asset] then
-        return unpack(importCache[asset])
-    end
-    
-    local assets 
+	if importCache[asset] then
+		return unpack(importCache[asset])
+	end
 
-    if asset:find("rbxassetid://") then
-        assets = { game:GetObjects(asset)[1] }
-    elseif web then
-        assets = { loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/Hydroxide/revision/" .. asset .. ".lua"), asset .. '.lua')() }
-    else
-        assets = { loadstring(readfile("hydroxide/" .. asset .. ".lua"), asset .. '.lua')() }
-    end
-    
-    importCache[asset] = assets
-    return unpack(assets)
+	local assets 
+
+	if string.match(asset, "rbxassetid://") then
+		assets = { unpack(game:GetObjects(asset)) }
+	elseif web then
+		assets = { loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/Hydroxide/revision/" .. asset .. ".lua"), asset .. '.lua')() }
+	else
+		assets = { loadstring(readfile("hydroxide/" .. asset .. ".lua"), asset .. '.lua')() }
+	end
+
+	importCache[asset] = assets
+	return unpack(assets)
 end
 
 local function hasMethods(methods)
-    for name in pairs(methods) do
-        if not environment[name] then
-            return false
-        end
-    end
+	for name in pairs(methods) do
+		if not environment[name] then
+			return false
+		end
+	end
 
-    return true
+	return true
 end
 
 local function useMethods(module)
-    for name, method in pairs(module) do
-        if method then
-            environment[name] = method
-        end
-    end
+	for name, method in pairs(module) do
+		if method then
+			environment[name] = method
+		end
+	end
 end
 
 if Window and PROTOSMASHER_LOADED then
-    getgenv().get_script_function = nil
+	getgenv().get_script_function = nil
 end
 
 local globalMethods = {
-    checkCaller = checkcaller,
-    newCClosure = newcclosure,
-    hookFunction = hookfunction or detour_function,
-    getGc = getgc or get_gc_objects,
-    getInfo = debug.getinfo or getinfo,
-    getSenv = getsenv,
-    getMenv = getmenv or getsenv,
-    getContext = getthreadcontext or get_thread_context or (syn and syn.get_thread_identity),
-    getConnections = get_signal_cons or getconnections,
-    getScriptClosure = getscriptclosure or get_script_function,
-    getNamecallMethod = getnamecallmethod or get_namecall_method,
-    getCallingScript = getcallingscript or get_calling_script,
-    getLoadedModules = getloadedmodules or get_loaded_modules,
-    getConstants = debug.getconstants or getconstants or getconsts,
-    getUpvalues = debug.getupvalues or getupvalues or getupvals,
-    getProtos = debug.getprotos or getprotos,
-    getStack = debug.getstack or getstack,
-    getConstant = debug.getconstant or getconstant or getconst,
-    getUpvalue = debug.getupvalue or getupvalue or getupval,
-    getProto = debug.getproto or getproto,
-    getMetatable = getrawmetatable or debug.getmetatable,
-    setClipboard = setclipboard or writeclipboard,
-    setConstant = debug.setconstant or setconstant or setconst,
-    setContext = setthreadcontext or set_thread_context or (syn and syn.set_thread_identity),
-    setUpvalue = debug.setupvalue or setupvalue or setupval,
-    setStack = debug.setstack or setstack,
-    setReadOnly = setreadonly or (make_writeable and function(table, readonly) if readonly then make_readonly(table) else make_writeable(table) end end),
-    isLClosure = islclosure or is_l_closure or (iscclosure and function(closure) return not iscclosure(closure) end),
-    isReadOnly = isreadonly or is_readonly,
-    isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or iselectronfunction or checkclosure,
+	checkCaller = checkcaller,
+	newCClosure = newcclosure,
+	hookFunction = hookfunction or detour_function,
+	getGc = getgc or get_gc_objects,
+	getInfo = debug.getinfo or getinfo,
+	getSenv = getsenv,
+	getMenv = getmenv or getsenv,
+	getContext = getthreadcontext or get_thread_context or (syn and syn.get_thread_identity),
+	getConnections = get_signal_cons or getconnections,
+	getScriptClosure = getscriptclosure or get_script_function,
+	getNamecallMethod = getnamecallmethod or get_namecall_method,
+	getCallingScript = getcallingscript or get_calling_script,
+	getLoadedModules = getloadedmodules or get_loaded_modules,
+	getConstants = debug.getconstants or getconstants or getconsts,
+	getUpvalues = debug.getupvalues or getupvalues or getupvals,
+	getProtos = debug.getprotos or getprotos,
+	getStack = debug.getstack or getstack,
+	getConstant = debug.getconstant or getconstant or getconst,
+	getUpvalue = debug.getupvalue or getupvalue or getupval,
+	getProto = debug.getproto or getproto,
+	getMetatable = getrawmetatable or debug.getmetatable,
+	setClipboard = setclipboard or writeclipboard,
+	setConstant = debug.setconstant or setconstant or setconst,
+	setContext = setthreadcontext or set_thread_context or (syn and syn.set_thread_identity),
+	setUpvalue = debug.setupvalue or setupvalue or setupval,
+	setStack = debug.setstack or setstack,
+	setReadOnly = setreadonly or (make_writeable and function(table, readonly) if readonly then make_readonly(table) else make_writeable(table) end end),
+	isLClosure = islclosure or is_l_closure or (iscclosure and function(closure) return not iscclosure(closure) end),
+	isReadOnly = isreadonly or is_readonly,
+	isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or iselectronfunction or checkclosure,
 }
 
 if PROTOSMASHER_LOADED ~= nil then
-    globalMethods.getConstant = function(closure, index)
-        return globalMethods.getConstants(closure)[index]
-    end
+	globalMethods.getConstant = function(closure, index)
+		return globalMethods.getConstants(closure)[index]
+	end
 end
 
 local oldGetUpvalue = globalMethods.getUpvalue
 local oldGetUpvalues = globalMethods.getUpvalues
 
 globalMethods.getUpvalue = function(closure, index)
-    if type(closure) == "table" then
-        return oldGetUpvalue(closure.Data, index)
-    end
+	if type(closure) == "table" then
+		return oldGetUpvalue(closure.Data, index)
+	end
 
-    return oldGetUpvalue(closure, index)
+	return oldGetUpvalue(closure, index)
 end
 
 globalMethods.getUpvalues = function(closure)
-    if type(closure) == "table" then
-        return oldGetUpvalues(closure.Data)
-    end
+	if type(closure) == "table" then
+		return oldGetUpvalues(closure.Data)
+	end
 
-    return oldGetUpvalues(closure)
+	return oldGetUpvalues(closure)
 end
 
 environment.import = import
 environment.hasMethods = hasMethods
 environment.oh = {
-    Events = {},
-    Hooks = {},
-    Methods = globalMethods,
-    Constants = {
-        Types = {
-            ["nil"] = "rbxassetid://4800232219",
-            table = "rbxassetid://4666594276",
-            string = "rbxassetid://4666593882",
-            number = "rbxassetid://4666593882",
-            boolean = "rbxassetid://4666593882",
-            userdata = "rbxassetid://4666594723",
-            ["function"] = "rbxassetid://4666593447",
+	Events = {},
+	Hooks = {},
+	Methods = globalMethods,
+	Constants = {
+		Types = {
+			["nil"] = "rbxassetid://4800232219",
+			table = "rbxassetid://4666594276",
+			string = "rbxassetid://4666593882",
+			number = "rbxassetid://4666593882",
+			boolean = "rbxassetid://4666593882",
+			userdata = "rbxassetid://4666594723",
+			["function"] = "rbxassetid://4666593447",
 
-            ["integral"] = "rbxassetid://4666593882"
-        },
-        Syntax = {
-            ["nil"] = Color3.fromRGB(244, 135, 113),
-            table = Color3.fromRGB(225, 225, 225),
-            string = Color3.fromRGB(225, 150, 85),
-            number = Color3.fromRGB(170, 225, 127),
-            boolean = Color3.fromRGB(127, 200, 255),
-            userdata = Color3.fromRGB(225, 225, 225),
-            ["function"] = Color3.fromRGB(225, 225, 225),
-            ["unnamed_function"] = Color3.fromRGB(175, 175, 175)
-        }
-    },
-    Exit = function()
-        for _i, event in pairs(oh.Events) do
-            event:Disconnect()
-        end
+			["integral"] = "rbxassetid://4666593882"
+		},
+		Syntax = {
+			["nil"] = Color3.fromRGB(244, 135, 113),
+			table = Color3.fromRGB(225, 225, 225),
+			string = Color3.fromRGB(225, 150, 85),
+			number = Color3.fromRGB(170, 225, 127),
+			boolean = Color3.fromRGB(127, 200, 255),
+			userdata = Color3.fromRGB(225, 225, 225),
+			["function"] = Color3.fromRGB(225, 225, 225),
+			["unnamed_function"] = Color3.fromRGB(175, 175, 175)
+		}
+	},
+	Exit = function()
+		for _, event in pairs(oh.Events) do
+			event:Disconnect()
+		end
 
-        for original, hook in pairs(oh.Hooks) do
-            local hookType = type(hook)
-            if hookType == "function" then
-                hookFunction(hook, original)
-            elseif hookType == "table" then
-                hookFunction(hook.Closure.Data, hook.Original)
-            end
-        end
+		for original, hook in pairs(oh.Hooks) do
+			local hookType = type(hook)
+			if hookType == "function" then
+				hookFunction(hook, newcclosure(original))
+			elseif hookType == "table" then
+				hookFunction(hook.Closure.Data, newcclosure(hook.Original))
+			end
+		end
 
-        local gmt = getrawmetatable(game)
+		local gmt = getrawmetatable(game)
 
-        if isReadOnly(gmt) then
-            setReadOnly(gmt, false) 
-        end
+		if isReadOnly(gmt) then
+			setReadOnly(gmt, false) 
+		end
 
-        if oh.Namecall then
-            gmt.__namecall = oh.Namecall
-        end
+		if oh.Namecall then
+			gmt.__namecall = oh.Namecall
+		end
 
-        local ui = importCache["rbxassetid://5042109928"]
-        local assets = importCache["rbxassetid://5042114982"]
+		local ui = importCache["rbxassetid://5042109928"]
+		local assets = importCache["rbxassetid://5042114982"]
 
-        if ui then
-            unpack(ui):Destroy()
-        end
+		if ui then
+			unpack(ui):Destroy()
+		end
 
-        if assets then
-            unpack(assets):Destroy()
-        end
-    end
+		if assets then
+			unpack(assets):Destroy()
+		end
+	end
 }
 
 if getConnections then 
-    for __, connection in pairs(getConnections(game:GetService("ScriptContext").Error)) do
+	for _, connection in pairs(getConnections(game:GetService("ScriptContext").Error)) do
 
-        local conn = getrawmetatable(connection)
-        local old = conn.__index
-        if PROTOSMASHER_LOADED ~= nil then setWriteable(conn) else setReadOnly(conn, false) end
-        conn.__index = newcclosure(function(t, k)
-            if k == "Connected" then
-                return true
-            end
-            return old(t, k)
-        end)
+		local conn = getrawmetatable(connection)
+		local old = conn.__index
+		if PROTOSMASHER_LOADED ~= nil then setWriteable(conn) else setReadOnly(conn, false) end
+		conn.__index = newcclosure(function(t, k)
+			if k == "Connected" then
+				return true
+			end
+			return old(t, k)
+		end)
 
-        if PROTOSMASHER_LOADED ~= nil then
-            setReadOnly(conn)
-            connection:Disconnect()
-        else
-            setReadOnly(conn, true)
-            connection:Disable()
-        end
-    end
+		if PROTOSMASHER_LOADED ~= nil then
+			setReadOnly(conn)
+			connection:Disconnect()
+		else
+			setReadOnly(conn, true)
+			connection:Disable()
+		end
+	end
 end
 
 useMethods(globalMethods)
