25c6f42a05ffd43eb90bcf3c55411fa8b02eb717
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-22 13:10:15 +0200

    Update ohaux.lua

--- ohaux.lua
+++ ohaux.lua
@@ -7,43 +7,41 @@ local getConstants = debug.getconstants or getconstants or getconsts
 local isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or checkclosure
 local isLClosure = islclosure or is_l_closure or (iscclosure and function(f) return not iscclosure(f) end)
 
-assert(getGc and getInfo and getConstants and isXClosure, "Your exploit is not supported")
+assert(getGc and getInfo and getConstants and isXClosure, "Your executor is not supported")
 
 local function matchConstants(closure, list)
-    if not list then
-        return true
-    end
-    
-    local constants = getConstants(closure)
-    
-    for index in pairs(list) do
-        if not constants[index] then
-            return false
-        end
-    end
-    
-    return true
+	if not list then
+		return true
+	end
+
+	local constants = getConstants(closure)
+
+	for index in pairs(list) do
+		if not constants[index] then
+			return false
+		end
+	end
+
+	return true
 end
 
 local function searchClosure(script, name, upvalueIndex, constants)
-    for _i, v in pairs(getGc()) do
-        local parentScript = rawget(getfenv(v), "script")
+	for _, v in pairs(getGc()) do
+		local parentScript = rawget(getfenv(v), "script")
 
-        if type(v) == "function" and 
-            isLClosure(v) and 
-            not isXClosure(v) and 
-            (
-                (script == nil and parentScript.Parent == nil) or script == parentScript
-            ) 
-            and pcall(getUpvalue, v, upvalueIndex)
-        then
-            if ((name and name ~= "Unnamed function") and getInfo(v).name == name) and matchConstants(v, constants) then
-                return v
-            elseif (not name or name == "Unnamed function") and matchConstants(v, constants) then
-                return v
-            end
-        end
-    end
+		if type(v) == "function" and 
+		isLClosure(v) and 
+		not isXClosure(v) and 
+		((script == nil and parentScript.Parent == nil) or script == parentScript) 
+		and pcall(getUpvalue, v, upvalueIndex)
+		then
+			if ((name and name ~= "Unnamed function") and getInfo(v).name == name) and matchConstants(v, constants) then
+				return v
+			elseif (not name or name == "Unnamed function") and matchConstants(v, constants) then
+				return v
+			end
+		end
+	end
 end
 
 aux.searchClosure = searchClosure
