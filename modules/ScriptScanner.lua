89facd7b2f5fbd6b01e79b5d268c1ca4db335d79
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-04-29 17:15:10 +0300

    Update ScriptScanner.lua

--- modules/ScriptScanner.lua
+++ modules/ScriptScanner.lua
@@ -2,35 +2,35 @@ local ScriptScanner = {}
 local LocalScript = import("objects/LocalScript")
 
 local requiredMethods = {
-    ["getGc"] = true,
-    ["getSenv"] = true,
-    ["getProtos"] = true,
-    ["getConstants"] = true,
-    ["getScriptClosure"] = true,
-    ["isXClosure"] = true
+["getGc"] = true,
+["getSenv"] = true,
+["getProtos"] = true,
+["getConstants"] = true,
+["getScriptClosure"] = true,
+["isXClosure"] = true
 }
 
 local function scan(query)
-    local scripts = {}
-    query = query or ""
+	local scripts = {}
+	query = query or ""
 
-    for _i, v in pairs(getGc()) do
-        if type(v) == "function" and not isXClosure(v) then
-            local script = rawget(getfenv(v), "script")
+	for _, v in pairs(getGc()) do
+		if type(v) == "function" and not isXClosure(v) then
+			local script = rawget(getfenv(v), "script")
 
-            if typeof(script) == "Instance" and 
-                not scripts[script] and 
-                script:IsA("LocalScript") and 
-                script.Name:lower():find(query) and
-                getScriptClosure(script) and
-                pcall(function() getsenv(script) end)
-            then
-                scripts[script] = LocalScript.new(script)
-            end
-        end
-    end
+			if typeof(script) == "Instance" and 
+				not scripts[script] and 
+				script:IsA("LocalScript") and 
+				string.match(string.lower(script.Name), query) and
+				getScriptClosure(script) and
+				pcall(getsenv, script)
+			then
+				scripts[script] = LocalScript.new(script)
+			end
+		end
+	end
 
-    return scripts
+	return scripts
 end
 
 ScriptScanner.RequiredMethods = requiredMethods
