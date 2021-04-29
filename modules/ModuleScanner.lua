b835d57228cf013635848481340f654cad38cf43
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-04-29 17:07:52 +0300

    Update ModuleScanner.lua

--- modules/ModuleScanner.lua
+++ modules/ModuleScanner.lua
@@ -2,24 +2,24 @@ local ModuleScanner = {}
 local ModuleScript = import("objects/ModuleScript")
 
 local requiredMethods = {
-    ["getMenv"] = true,
-    ["getProtos"] = true,
-    ["getConstants"] = true,
-    ["getScriptClosure"] = true,
-    ["getLoadedModules"] = true
+	["getMenv"] = true,
+	["getProtos"] = true,
+	["getConstants"] = true,
+	["getScriptClosure"] = true,
+	["getLoadedModules"] = true
 }
 
 local function scan(query)
-    local modules = {}
-    query = query or ""
-    
-    for _i, module in pairs(getLoadedModules()) do
-        if module.Name:lower():find(query) then
-            modules[module] = ModuleScript.new(module)
-        end
-    end
+	local modules = {}
+	query = query or ""
 
-    return modules
+	for _, module in pairs(getLoadedModules()) do
+		if string.match(string.lower(module.Name), query) then
+			modules[module] = ModuleScript.new(module)
+		end
+	end
+
+	return modules
 end
 
 ModuleScanner.Scan = scan
