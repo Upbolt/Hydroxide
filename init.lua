9a44c03a18aab88bda7de22c7275ada1d64a99e2
NUTTREX <94196871+NUTTREX@users.noreply.github.com>
2022-04-22 22:13:26 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ if oh then
 end
 
 local web = true
-local user = "Upbolt" -- change if you're using a fork
+local user = "NUTREX" -- change if you're using a fork
 local branch = "revision"
 local importCache = {}
 
