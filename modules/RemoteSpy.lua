ec694b8c09942cae289435e19c5037729ad90438
strawberrys <56797207+strawbberrys@users.noreply.github.com>
2022-05-15 19:27:01 -0400

    Update RemoteSpy.lua

--- modules/RemoteSpy.lua
+++ modules/RemoteSpy.lua
@@ -97,6 +97,8 @@ local function checkPermission(instance)
     if (instance.ClassName) then end
 end
 
+local exec = identifyexecutor()
+
 for _name, hook in pairs(methodHooks) do
     local originalMethod
     originalMethod = hookFunction(hook, newCClosure(function(...)
@@ -127,7 +129,7 @@ for _name, hook in pairs(methodHooks) do
                 local call = {
                     script = getCallingScript((PROTOSMASHER_LOADED ~= nil and 2) or nil),
                     args = vargs,
-                    func = getInfo(3).func
+                    func = exec == "Synapse X" and getInfo(2).func or getInfo(3).func
                 }
     
                 remote:IncrementCalls(call)
