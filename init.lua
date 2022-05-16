bcc75799880c587be78e6c5a13c2e3ef18dac594
strawberrys <56797207+strawbberrys@users.noreply.github.com>
2022-05-16 01:45:28 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ end
 
 local web = true
 local user = "strawbberrys" -- change if you're using a fork
-local branch = "revision"
+local branch = "patch-1"
 local importCache = {}
 
 local function hasMethods(methods)
