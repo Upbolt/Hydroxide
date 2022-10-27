8b7af27e337c5a55dbff91353450ca09280c7f20
ARSON <58544287+Upbolt@users.noreply.github.com>
2022-10-26 21:39:04 -0700

    Update TabSelector.lua

--- ui/controls/TabSelector.lua
+++ ui/controls/TabSelector.lua
@@ -2,7 +2,7 @@ local TweenService = game:GetService("TweenService")
 
 local TabSelector = {}
 
-local Base = import("rbxassetid://5042109928").Base
+local Base = import("rbxassetid://11389137937").Base
 local Tabs = Base.Tabs.Container
 local Pages = Base.Body.Pages
 
